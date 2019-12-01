3944f567075618fec4b0bef1fa8f5e96d74c8124
wardis <wardi2976@live.fr>
2019-12-01 00:25:04 +0000

    black background

--- src/Card.js
+++ src/Card.js
@@ -5,11 +5,17 @@ const Card = () => {
   const [background, setBackground] = useState('#fdfdfd');
   const [font, setFont] = useState('#424242');
 
+  const setStyle = (background = '#fdfdfd', font = '#424242') => {
+    setBackground(background);
+    setFont(font);
+  };
+
   const card = css`
     max-width: 600px;
     padding: 40px;
     margin: 0 auto;
     background-color: ${background};
+    transition: all 1s;
     h1 {
       font-weight: 400;
       font-size: 36px;
@@ -57,7 +63,13 @@ const Card = () => {
         tenetur! Expedita quae at repudiandae fuga fugiat, nulla minima quia.
       </p>
       <div className={cardButtons}>
-        <button className={blackButton}>Black</button>
+        <button
+          onMouseEnter={() => setStyle('#424242', '#fdfdfd')}
+          onMouseOut={() => setStyle()}
+          className={blackButton}
+        >
+          Black
+        </button>
         <button className={blueButton}>Blue</button>
         <button className={yellowButton}>Yellow</button>
       </div>
