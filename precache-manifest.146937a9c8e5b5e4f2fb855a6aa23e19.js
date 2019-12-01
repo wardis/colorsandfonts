ece66fd72d840bcb4b2212e5b153e401774ddc3f
wardis <wardi2976@live.fr>
2019-12-01 00:44:46 +0000

    Updates

---
+++ precache-manifest.146937a9c8e5b5e4f2fb855a6aa23e19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09edd994fc898d945810fb5d998e929c",
+    "url": "/colorsandfonts/index.html"
+  },
+  {
+    "revision": "3a5187de8bfaf4dcf235",
+    "url": "/colorsandfonts/static/css/main.14e0d005.chunk.css"
+  },
+  {
+    "revision": "2f6a6fd05f82ff60f670",
+    "url": "/colorsandfonts/static/js/2.146173c9.chunk.js"
+  },
+  {
+    "revision": "3a5187de8bfaf4dcf235",
+    "url": "/colorsandfonts/static/js/main.cce13cf2.chunk.js"
+  },
+  {
+    "revision": "5a56d7a46a83ec2431f3",
+    "url": "/colorsandfonts/static/js/runtime-main.da1f8c10.js"
+  }
+]);
